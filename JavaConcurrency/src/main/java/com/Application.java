167ff1c95c9519609c498a605009f856f5eb965f
wutao-boy <37316169+wutao-boy@users.noreply.github.com>
2018-05-21 21:55:55 +0800

    springboot helloworld

---
+++ JavaConcurrency/src/main/java/com/Application.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        //spring应用启动
+        SpringApplication.run(Application.class,args);
+    }
+}
