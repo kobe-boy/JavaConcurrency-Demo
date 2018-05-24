be9943ec4d98f2d321cda2113222b22e2473346f
wutao-boy <37316169+wutao-boy@users.noreply.github.com>
2018-05-24 22:59:09 +0800

    单例

---
+++ JavaConcurrency/src/main/java/example/singleton/SingletonExample6.java
@@ -0,0 +1,34 @@
+package example.singleton;
+
+
+import com.annoations.ThreadSafe;
+
+/**
+ * 饿汉模式  静态代码是顺序执行的
+ * 单例实例在类装载时进行创建
+ */
+@ThreadSafe
+public class SingletonExample6 {
+
+    // 私有构造函数
+    private SingletonExample6() {
+
+    }
+
+    // 单例对象
+    private static SingletonExample6 instance = null;
+
+    static {
+        instance = new SingletonExample6();
+    }
+
+    // 静态的工厂方法
+    public static SingletonExample6 getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInstance().hashCode());
+        System.out.println(getInstance().hashCode());
+    }
+}
