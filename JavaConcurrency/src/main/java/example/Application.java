3a3a69e1b7a276250d2c2debfb04b6ebc52e58d8
wutao-boy <37316169+wutao-boy@users.noreply.github.com>
2018-05-27 17:55:12 +0800

    同步容器和并发容器

---
+++ JavaConcurrency/src/main/java/example/Application.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class Application extends WebMvcConfigurerAdapter {
+    public static void main(String[] args) {
+        //spring应用启动
+        SpringApplication.run(Application.class,args);
+    }
+
+    //过滤器
+    @Bean
+    public FilterRegistrationBean httpFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new HttpFilter());
+        registrationBean.addUrlPatterns("/threadLocal/*");
+        return registrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new example.HttpInterceptor()).addPathPatterns("/**");
+    }
+}
